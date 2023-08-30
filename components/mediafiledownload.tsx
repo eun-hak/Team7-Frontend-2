55ea98a05f38165dfbf44e285b154e7ddfe42225
조기범 <akfmzh1207@naver.com>
2023-08-30 21:32:48 +0900

    파일 유효성검사 테스트

--- components/mediafiledownload.tsx
+++ components/mediafiledownload.tsx
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { useRef, useState } from "react";
 import { useRecoilState_TRANSITION_SUPPORT_UNSTABLE } from "recoil";
 const MediaFileDownload = () => {
+  const [selectedFile, setSelectedFile] = useState<string | undefined>("null");
   const fileInputRef = useRef<HTMLInputElement>(null);
   const handleClickFileInput = () => {
     fileInputRef.current?.click();
@@ -12,17 +13,33 @@ const MediaFileDownload = () => {
     type: string;
   };
   const uploadProfile = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fileList = e.target.files;
-    const length = fileList?.length;
+    // const fileList = e.target.files;
+    // const length = fileList?.length;
     // const [imageFile, setImageFile] = useState(null);
-    if (fileList && fileList[0]) {
-      const url = URL.createObjectURL(fileList[0]);
-      //   setImageFile({
-      //     file: fileList[0],
-      //     thumbnail: url,
-      //     type: fileList[0].type.slice(0, 5),
-      //   });
+    const { files }: any = e.target;
+    // 파일 첨부 도중에 취소를 누를 경우 에러가 발생하지 않도록 return 처리
+    if (files.length <= 0) {
+      return;
     }
+    const file = files[0];
+    // file 객체에서 타입 확인
+    const fileType = file.type;
+    if (!fileType.includes("m4a")) {
+      alert(
+        `해당 파일은 이미지 파일이 아닙니다.\n이미지(JPG,JPEG,GIF,PNG)나 PDF 파일을 업로드 해주세요.`
+      );
+      e.target.value = "";
+
+      return;
+    }
+    // if (fileList && fileList[0]) {
+    //   const url = URL.createObjectURL(fileList[0]);
+    //   //   setImageFile({
+    //   //     file: fileList[0],
+    //   //     thumbnail: url,
+    //   //     type: fileList[0].type.slice(0, 5),
+    //   //   });
+    // }
   };
   return (
     <FileUploadContainer>
@@ -35,7 +52,8 @@ const MediaFileDownload = () => {
         />
         {/* <FileUploadButton type="button" onClick={handleClickFileInput}>
           파일 업로드 버튼
-        </FileUploadButton> */}
+        </FileUploadButton>
+      */}
       </FileUploadForm>
     </FileUploadContainer>
   );
