feb3f1347a33bb61ce278bba37f4f4adf78c3f58
조기범 <akfmzh1207@naver.com>
2023-08-30 17:04:00 +0900

    로컬스토리지 오류 해결

--- util/loginStorage.ts
+++ util/loginStorage.ts
@@ -1,35 +1,52 @@
-const storage = localStorage;
+// if (typeof window!== 'undefined'){
+//   const storage = localStorage;
+// }
 
+// 넥스트 전용 로컬스토리지
+
+// const localstorageLoading = () =>{
+//   if (typeof window!== 'undefined'){
+//       return
+//   }
+// }
 //로그인에 관련된 곳
 export const isLoginStorage = () => {
-  const state = localStorage.login;
-  return !!state;
+  if (typeof window !== "undefined") {
+    const state = localStorage.login;
+    return !!state;
+  }
   // return true;
 };
 
 export const getStorage = (key: string, defaultValue = undefined) => {
-  try {
-    const storedValue = storage.getItem(key);
-    return storedValue || defaultValue;
-  } catch (error) {
-    console.error(error);
-    return defaultValue;
+  if (typeof window !== "undefined") {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue || defaultValue;
+    } catch (error) {
+      console.error(error);
+      return defaultValue;
+    }
   }
 };
 
 export const setStorage = (key: string, value: string | [string, unknown]) => {
-  try {
-    storage.setItem(key, JSON.stringify(value));
-  } catch (error) {
-    console.error(error);
+  if (typeof window !== "undefined") {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
 
 // 로그아웃 시
 export const removeStorage = (key: string) => {
-  try {
-    storage.removeItem(key);
-  } catch (error) {
-    console.error(error);
+  if (typeof window !== "undefined") {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
