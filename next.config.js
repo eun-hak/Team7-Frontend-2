bce9bf5496a886a686b6b7d20a88e9ddffc40131
조기범 <akfmzh1207@naver.com>
2023-09-12 19:30:57 +0900

    재설정

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://musicvillains.run.goorm.io/:path*",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
+
 const path = require("path");
 module.exports = nextConfig;
 
