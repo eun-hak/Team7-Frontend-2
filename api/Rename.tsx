511c11d0ec80f75550609ab9c778af58fa7b5ebc
조기범 <akfmzh1207@naver.com>
2023-09-15 17:30:51 +0900

    닉네임 변경api 개발

---
+++ api/Rename.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import JwtInterceptors, { baseURL } from "./ApiController";
+import { getStorage } from "@/util/loginStorage";
+import { usePathname, useRouter } from "next/navigation";
+
+const ReName = () => {
+  //   const { instance } = JwtInterceptors();
+  const path = usePathname();
+  const router = useRouter();
+  const parts = path.split("/"); // 경로를 '/' 문자로 분리
+  const lastPart = parts[parts.length - 1]; // 마지막 부분을 가져오기
+  const memberId = getStorage("member")?.replace(/\"/gi, "");
+  const rename = async (data: string) => {
+    const token: any = getStorage("access");
+    const token2 = `Bearer ${token.replace(/\"/gi, "")}`;
+    try {
+      const response = await axios.post(
+        `${baseURL}members/${memberId}?name=${data}`,
+        undefined,
+        {
+          headers: {
+            Authorization: token2,
+          },
+        }
+      );
+      console.log(response);
+      alert("닉네임 수정이 완료되었습니다");
+      router.push("/main?value=전체");
+      return response.data;
+    } catch (error) {
+      console.error("Error getting access token:", error);
+      throw error;
+    }
+  };
+
+  return { rename };
+};
+
+export default ReName;
