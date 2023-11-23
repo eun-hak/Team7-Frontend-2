73fa7f74f098f802fa4bdc87274f4b5049ac6ccb
eunhak <akfmzh1207@naver.com>
2023-11-23 20:48:42 +0900

    fix: main화면 배경 추가

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,11 +2,28 @@
 import styled from "@emotion/styled";
 import "../globals.css";
 import { Providers } from "../providers";
+import { useSearchParams } from "next/navigation";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  interface BackgroundImages {
+    [key: string]: string;
+  }
+
+  const backgroundImages: BackgroundImages = {
+    전체: "#F5F5F5",
+    고음괴물: "#E40C0C",
+    화음귀신: "#E4F0F5",
+    힙합전사: "#15B28D",
+    하이라이트도둑: "#2962FF",
+    소몰이대장: "#795548",
+    삑사리요정: "#FF5CBE",
+    기타: "#F5F5F5",
+  };
+  const searchParams = useSearchParams();
+  const searchValue = searchParams.get("value") || "";
   return (
     <BodyStyle>
-      <BgStyle>
+      <BgStyle background={backgroundImages[searchValue]}>
         <Providers>{children}</Providers>
       </BgStyle>
     </BodyStyle>
@@ -17,8 +34,10 @@ export default RootLayout;
 
 const BodyStyle = styled.body``;
 
-const BgStyle = styled.div`
-  background-color: #fff;
+const BgStyle = styled.div<{ background: string }>`
+  /* background-color: #fff; */
+  background-color: ${(props) => props.background};
+
   min-height: calc(100vh * 1);
   overflow-y: hidden;
 `;
